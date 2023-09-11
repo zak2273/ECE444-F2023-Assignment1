ce4e64e39a85fd9a61cc005a049f96f0859fb4bd
Zakria Nabi <zakria.nabi@mail.utoronto.ca>
2023-09-10 20:44:35 -0400

    Adding screenshots of commits.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ![Alt text](<1 Adding README.png>)
 ![Alt text](<2 Merging.png>)
 ![Alt text](<3 Merge conflict.png>)
+![Alt text](<4 Commit utils.png>)
+![Alt text](<5 Commit utils_test.png>)
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 class utils:
     def __init__(self, num):
         self.num = num
+        
     def reversed(self):
-        while self.num != 0:
-            digit = self.num % 10
-            reversed_num = reversed_num * 10 + digit
-            self.num //= 10
+        number = int(self.num)
+        reversed_num = 0
+        while number != 0:
+            digit = number % 10
+            reversed_num = (reversed_num * 10) + digit
+            number //= 10
+        return reversed_num
 
     def formatter(self):
-        return (bin(self.num), oct(self.num))
+        number = int(self.num)
+        return (bin(number), oct(number))
